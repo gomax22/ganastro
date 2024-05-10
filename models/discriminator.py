735956f4070c00ccfb57799528661794f87c8715
gomax22 <giordanoorsini.massimiliano@gmail.com>
2024-05-10 13:39:29 +0200

    first version of discriminator and generator (dcgan, full image size)

---
+++ models/discriminator.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+class DiscriminatorBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
+        super(DiscriminatorBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        
+    def forward(self, x):
+        return self.block(x)
+    
+    
+class Discriminator(nn.Module):
+    def __init__(self, num_features, color_channels, n_layers=6):
+        super(Discriminator, self).__init__()
+        self.num_features = num_features
+        self.color_channels = color_channels
+        self.n_layers = n_layers
+        
+        self.net = nn.ModuleList()
+        self.net.append(DiscriminatorBlock(color_channels, num_features, 4, 2, 3))
+        
+        layers = [2 ** i for i in range(0, n_layers)]
+        self.net.append([DiscriminatorBlock(num_features * layers[i], num_features * layers[i+1], 4, 2, 1) for i in range(n_layers)])
+        self.net.append(nn.Sequential(
+            nn.Conv2d(num_features * (2 ** n_layers), 1, 6, 1, 0),
+            nn.Sigmoid()
+        ))
+
+
+    def forward(self, x):
+        for layer in self.net:
+            x = layer(x)
+        return x
---
+++ models/generator.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class GeneratorBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=False):
+        super(GeneratorBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
+        
+    def forward(self, x):
+        return self.block(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, num_features, latent_dim, color_channels, n_layers=6):
+        super(Generator, self).__init__()
+        self.latent_dim = latent_dim
+        self.num_features = num_features
+        self.color_channels = color_channels
+        self.n_layers = n_layers    
+
+        self.net = nn.ModuleList()
+        self.net.append(GeneratorBlock(latent_dim, num_features * (2 ** n_layers), 4, 1, 0))
+        
+        layers = [2 ** i for i in range(n_layers, 0, -1)]
+        self.net.append([GeneratorBlock(num_features * layers[i], num_features * layers[i-1], 4, 2, 1) for i in range(n_layers)])
+        self.net.append(nn.Sequential(
+            nn.ConvTranspose2d(num_features, color_channels, 4, 2, 4, bias=False),
+            nn.Tanh()
+        ))
+        
+
+    def forward(self, x):
+        for layer in self.net:
+            x = layer(x)
+        return x
+
